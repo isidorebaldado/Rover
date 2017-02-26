913d85e92f72e4347250c6c9ec1cc5c2d6f91a90
Isidore Baldado <isidore@Isidores-MBP.attlocal.net>
2017-02-26 18:45:50 -0500

    adding the initial rover app

---
+++ rover_app/rover.py
@@ -0,0 +1,28 @@
+import time
+from itertools import cycle
+from flask import Flask, render_template
+
+import RPi.GPIO as GPIO
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World"
+
+@app.route("/forward")
+def forward():
+    return "Moving forward"
+
+@app.route("/stop")
+def stop():
+    return "Moving backward"
+
+@app.route("/stream")
+def hello():
+    return "Streaming video"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=500)
